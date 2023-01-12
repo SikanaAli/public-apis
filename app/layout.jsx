88775f665c58087fbcba19e27f01b7c17f5faf79
ImmortalDev <34840787+SikanaAli@users.noreply.github.com>
2023-01-12 21:27:05 +0200

    added analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Suspense } from 'react'
+import { AnalyticsWrapper } from '../components/analytics'
 
 export default function RootLayout({ children }) {
   return (
@@ -10,6 +11,7 @@ export default function RootLayout({ children }) {
       */}
       <head />
       <body>{children}</body>
+      <AnalyticsWrapper/>
     </html>
   )
 }
